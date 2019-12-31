bca896c7355e0c7ac2447284afb8541505b098c0
Pavel Beletsky <pavel.beletsky@live.com>
2019-12-31 09:51:22 -0500

    added actions to controller, improved sorting in model

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,8 +1,33 @@
 class LeadersController < ApplicationController
   
+  # GET /leaders/
   def index
     leaders = Leader.top_ten
     render json: leaders, except: [:created_at, :updated_at]
+  end
+
+  # POST /leaders/
+  def create
+    # @leader = Leader.create!(player_params)
+    # json_response(@leader, :created)
+    leader = Leader.create!(player_params)
+    render json: leader, except: [:created_at, :updated_at]
+  end
+
+  # PUT /leaders/:id
+  def update
+    # @leader.update(player_params)
+    # head :no_content
+    leader = Leader.find(params[:id])
+    leader.update(player_params)
+    render json: leader, except: [:created_at, :updated_at]
+  end
+
+  private
+
+  def player_params
+    # whitelist params
+    params.permit(:name, :avatar, :wins, :losses, :accuracy, :id)
   end
 
 end
--- app/models/leader.rb
+++ app/models/leader.rb
@@ -1,14 +1,27 @@
 class Leader < ApplicationRecord
 
+  # def self.top_ten
+  #   arr_top_ten = []
+  #   hash_id_score = {}
+  #   Leader.find_each do |leader|
+  #     hash_id_score[leader.id] = leader.wins - leader.losses
+  #   end
+  #   arr_id_score = hash_id_score.sort_by {|k, v| -v}.first(10)
+  #   arr_id_score.each do |i|
+  #     arr_top_ten.push(Leader.find(i[0]))
+  #   end
+  #   arr_top_ten
+  # end
+
   def self.top_ten
     arr_top_ten = []
     hash_id_score = {}
 
     Leader.find_each do |leader|
-      hash_id_score[leader.id] = leader.wins - leader.losses
+      hash_id_score[leader.id] = [leader.wins - leader.losses, leader.accuracy]
     end
 
-    arr_id_score = hash_id_score.sort_by {|k, v| -v}.first(10)
+    arr_id_score = hash_id_score.sort_by {|k, v| [-v[0], -v[1]]}.first(10)
 
     arr_id_score.each do |i|
       arr_top_ten.push(Leader.find(i[0]))
