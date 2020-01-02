0aeb8719e6f5e67ab882c516975037f93069c3bd
Pavel Beletsky <pavel.beletsky@live.com>
2020-01-02 18:10:08 -0500

    leaderboarb fix for zero players

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -1,24 +1,13 @@
 class Leader < ApplicationRecord
 
-  # def self.top_ten
-  #   arr_top_ten = []
-  #   hash_id_score = {}
-  #   Leader.find_each do |leader|
-  #     hash_id_score[leader.id] = leader.wins - leader.losses
-  #   end
-  #   arr_id_score = hash_id_score.sort_by {|k, v| -v}.first(10)
-  #   arr_id_score.each do |i|
-  #     arr_top_ten.push(Leader.find(i[0]))
-  #   end
-  #   arr_top_ten
-  # end
-
   def self.top_ten
     arr_top_ten = []
     hash_id_score = {}
 
     Leader.find_each do |leader|
-      hash_id_score[leader.id] = [leader.wins - leader.losses, leader.accuracy]
+      if (leader.wins != 0 || leader.losses != 0)
+        hash_id_score[leader.id] = [leader.wins - leader.losses, leader.accuracy]
+      end
     end
 
     arr_id_score = hash_id_score.sort_by {|k, v| [-v[0], -v[1]]}.first(10)
