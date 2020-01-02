e0a5fec0a1b528a3eb8994bb0feaccae2938184f
Pavel Beletsky <pavel.beletsky@live.com>
2020-01-01 23:12:29 -0500

    final

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -8,16 +8,12 @@ class LeadersController < ApplicationController
 
   # POST /leaders/
   def create
-    # @leader = Leader.create!(player_params)
-    # json_response(@leader, :created)
     leader = Leader.create!(player_params)
     render json: leader, except: [:created_at, :updated_at]
   end
 
   # PUT /leaders/:id
   def update
-    # @leader.update(player_params)
-    # head :no_content
     leader = Leader.find(params[:id])
     leader.update(player_params)
     render json: leader, except: [:created_at, :updated_at]
@@ -26,7 +22,6 @@ class LeadersController < ApplicationController
   private
 
   def player_params
-    # whitelist params
     params.permit(:name, :avatar, :wins, :losses, :accuracy, :id)
   end
 
